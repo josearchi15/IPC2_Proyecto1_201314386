a6d34d9f718bf9ffcc735268f5aeb7afde686c5d
jose carlos archila galicia <chijolx3@gmail.com>
2021-08-07 15:30:31 -0600

    se realiza modificacion en index

--- index.py
+++ index.py
@@ -7,9 +7,9 @@ for terreno in root:
     print(terreno.get("nombre"))
     xCurrent = 1
     xLine = list()
-    tab = Tablero()
 
     for el in terreno:
+        tab = Tablero()
 
 
         if el.tag == "posicioninicio":
@@ -25,18 +25,17 @@ for terreno in root:
             print(labelEnd)
         else:
             if int(el.attrib.get("x")) == xCurrent:
-                print(int(el.attrib.get("x")),"--->",xCurrent)
-                # print(el.attrib.get("x"),el.attrib.get("y"))
-                # print(el.text)
                 xLine.append(int(el.text))
+                # print(int(el.attrib.get("x")),"--->",xCurrent)
+                # print(el.attrib.get("x"),el.attrib.get("y"))
                 # print("x = current")
             else:
                 print("________________________________",xLine)
                 tab.addY(xLine)
                 xLine.clear()
-                print("________________________________",xLine)
+                # print("________________________________",xLine)
                 xLine.append(int(el.text))
                 xCurrent+=1
                 print(int(el.attrib.get("x")),"--->",xCurrent)
             
-    tab.show()
+        tab.show()
